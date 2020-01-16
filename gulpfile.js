2059249a1b4c6e625dbc319ae8eeb1f523a40f70
Elsayed <sayedaladway@gamil.com>
2020-01-16 10:13:25 +0200

    completed large hero block and intro content block

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+var gulp = require("gulp");
+var plumber = require("gulp-plumber"); //prevent errors form breaking gulp watch
+var gutil = require("gulp-util"); //prevent errors form breaking gulp watch
+var imagemin = require("gulp-imagemin");
+var uglify = require("gulp-uglify");
+var sass = require("gulp-sass");
+var concat = require("gulp-concat");
+var sourcemaps = require("gulp-sourcemaps");
+var connect = require("gulp-connect");
+
+// copy all html files
+gulp.task("copyHtml", function() {
+  gulp
+    .src("./app/*.html")
+    .pipe(gulp.dest("./dist"))
+    .pipe(connect.reload());
+});
+
+// optimize images
+gulp.task("imageMin", function() {
+  gulp
+    .src("./app/assets/images/**")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./dist/imgs"));
+});
+
+//Compile Sass
+gulp.task("sass", function() {
+  gulp
+    .src("./app/assets/css/**/*.scss")
+    //prevent errors form breaking gulp watch
+    .pipe(
+      plumber(function(error) {
+        gutil.log(error.message);
+        this.emit("end");
+      })
+    )
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        includePaths: require("node-normalize-scss").includePaths
+      })
+    )
+    .pipe(sass(/*{ outputStyle: "compressed" }*/))
+    .pipe(sourcemaps.write("."))
+
+    .pipe(gulp.dest("dist/css"))
+    .pipe(connect.reload());
+});
+
+// scripts
+gulp.task("scripts", function() {
+  gulp
+    .src("./app/assets/js/*.js")
+    .pipe(concat("script.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/js"))
+    .pipe(connect.reload());
+});
+
+// livereload
+gulp.task("connect", function() {
+  connect.server({
+    root: "dist",
+    livereload: true
+  });
+});
+
+// watch
+gulp.task("watch", function() {
+  gulp.watch("./app/*.html", ["copyHtml"]);
+  gulp.watch("./app/assets/images/**", ["imageMin"]);
+  gulp.watch("./app/assets/css/**/*.scss", ["sass"]);
+  gulp.watch("./app/assets/js/*.js", ["scripts"]);
+});
+
+gulp.task("default", ["watch", "connect"]);

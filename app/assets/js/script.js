a91ea6d0c5acba8289ed18cbdd64f6fd20d97e68
Elsayed <sayedaladway@gamil.com>
2020-01-22 17:04:20 +0200

    complete the reveal on scroll behaviour

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,3 +1,7 @@
 import MobilMenu from "./modules/MobilMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
+import $ from "jquery";
 
 var mobilMenu = new MobilMenu(); // new instance of object
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
